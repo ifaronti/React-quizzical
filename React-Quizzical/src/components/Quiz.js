585910837116c29f801027bfc61339a9466a4aee
ifaronti <135629496+ifaronti@users.noreply.github.com>
2023-10-11 14:34:51 +0100

    Delete React-Quizzical/src/components directory

--- React-Quizzical/src/components/Quiz.js
+++
@@ -1,9 +0,0 @@
-
-export default function Quiz(props){
-    let questionString = props.question
-    return(
-        <div className="question">
-            <h4>{questionString}</h4>
-        </div>
-    )
-}
